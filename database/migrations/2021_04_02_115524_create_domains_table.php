6ff9185113d5688e19cb286192babf28ff662848
Denis Kashchavtsev <dkashavtsev@gmail.com>
2023-03-06 19:06:11 +0200

    running tests

--- database/migrations/2021_04_02_115524_create_domains_table.php
+++ database/migrations/2021_04_02_115524_create_domains_table.php
@@ -37,8 +37,31 @@ class CreateDomainsTable extends Migration
             $table->string('contact_name', 255)->nullable();
             $table->text('additional_notes')->nullable();
         });
-        
-        \DB::statement('CREATE TABLE domain_temp_imports LIKE domains');
+
+        Schema::create('domain_temp_imports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('domain', 255)->default('');
+            $table->string('niche', 255)->nullable();
+            $table->string('lang', 32)->nullable();
+            $table->string('title', 1023)->nullable();
+
+            $table->unsignedInteger('ahrefs_dr')->nullable();
+            $table->unsignedInteger('ahrefs_traffic')->nullable();
+            $table->unsignedInteger('linked_domains')->nullable();
+            $table->unsignedInteger('ref_domains')->nullable();
+            $table->unsignedInteger('num_organic_keywords_top_10')->nullable();
+
+            $table->string('article_by', 255)->nullable();
+            $table->decimal('price', 7, 2)->nullable();
+            $table->string('sponsored_label', 255)->nullable();
+            $table->string('type_of_publication', 255)->nullable();
+            $table->string('type_of_link', 255)->nullable();
+            $table->string('contact_email', 255)->nullable();
+            $table->text('contact_form_link')->nullable();
+            $table->string('contact_name', 255)->nullable();
+            $table->text('additional_notes')->nullable();
+        });
     }
 
     /**
