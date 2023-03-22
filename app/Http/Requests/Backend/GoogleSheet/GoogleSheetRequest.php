0af7260bbdf3bff78f8f8ac8b8f962fa2b67536a
Denis Kashchavtsev <dkashavtsev@gmail.com>
2023-03-22 09:37:09 +0200

    change table

--- app/Http/Requests/Backend/GoogleSheet/GoogleSheetRequest.php
+++ app/Http/Requests/Backend/GoogleSheet/GoogleSheetRequest.php
@@ -3,8 +3,11 @@
 
 namespace App\Http\Requests\Backend\GoogleSheet;
 
+use App\Models\Domain;
+use App\Models\GoogleSheet;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class GoogleSheetRequest extends FormRequest
 {
@@ -26,7 +29,18 @@ class GoogleSheetRequest extends FormRequest
      */
     public function rules(): array
     {
+        if (isset($this->route()->parameters['google_sheet'])) {
+            return [
+                'id' => ['sometimes'],
+                'name' => ['required', 'string'],
+                'url' => ['required', 'string', Rule::unique('google_sheets', 'url')->ignore($this->route()->parameters['google_sheet']),],
+                'associations' => ['required', 'array'],
+                'associations.domain' => ['required', 'string', 'not_in:-- not use --'],
+                'import' => ['sometimes']
+            ];
+        }
         return [
+            'id' => ['sometimes'],
             'name' => ['required', 'string'],
             'url' => ['required', 'string', 'unique:google_sheets'],
             'associations' => ['required', 'array'],
